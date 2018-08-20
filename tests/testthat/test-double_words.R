f8de5b16f94379d76a684428864f84398bd586d2
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-20 14:03:55 +0100

    More tests for double words

--- tests/testthat/test-double_words.R
+++ tests/testthat/test-double_words.R
@@ -1,8 +1,11 @@
-testMatch <- list(
-  wordList = c('this', 'is', 'or', 'not', 'a', 'test') %>% list,
-  indexes = c(2, 4, 6, 8, 10, 12) %>% list,
-  tokenList =  c('s', 'w', 's', 'w', 's', 'w', 's', 'w', 's', 'w', 's', 'w') %>% list
+
+# Initialise list
+arg <- "this is or not a test"
+testMatch <- detectR::parse_words(
+  tokens = arg %>% detectR::tokenization(),
+  args = arg
 )
+
 
 # Begin tests
 test_that("Test that a double barrel sqli word can adjust the matching token information.", {
@@ -11,6 +14,28 @@ test_that("Test that a double barrel sqli word can adjust the matching token inf
   result <- testMatch %>% detectR::double_words()
 
   expect_equal( result$wordList[[1]][3], 'ornot' )
+  expect_equal( result$indexes[[1]], testMatch$indexes %>% `[[`(1) %>% `[`(1:(charLen - 1)) )
+
+})
+
+test_that("Double barrel word is caught at the start", {
+
+  testMatch$wordList[[1]] <- c('or', 'not', 'this', 'is', 'a', 'test')
+  charLen <- testMatch$wordList[[1]] %>% length
+  result <- testMatch %>% detectR::double_words()
+
+  expect_equal( result$wordList[[1]][1], 'ornot' )
+  expect_equal( result$indexes[[1]], testMatch$indexes %>% `[[`(1) %>% `[`(1:(charLen - 1)) )
+
+})
+
+test_that("Double barrel word is caught at the end", {
+
+  testMatch$wordList[[1]] <- c('this', 'is', 'a', 'test', 'or', 'not')
+  charLen <- testMatch$wordList[[1]] %>% length
+  result <- testMatch %>% detectR::double_words()
+
+  expect_equal( result$wordList[[1]][5], 'ornot' )
   expect_equal( result$indexes[[1]], testMatch$indexes %>% `[[`(1) %>% `[`(1:(charLen - 1)) )
 
 })
