9b84c3bad19f38739ae6c9ccf69260a78c0449de
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-20 21:06:46 +0100

    Clean up main feature investigation

--- R/features.R
+++ R/features.R
@@ -4,22 +4,21 @@
 
 features <- function(args) {
 
-  # Initialise attack keywords
-  attackWords <- detectR::attack_keywords()
-  attackNames <- attackWords %>% names
-
   # Tokenize stuff
   args %<>% tolower
-  tokens <- args %>% detectR::tokenization()
+  tokens <- args %>%
+    detectR::tokenization()
 
   # Create character distribution
-  chardist <- args %>% detectR::char_dist()
+  chardist <- args %>%
+    detectR::char_dist()
 
   # Split by spaces / collapse string / create a matrix of column 1
   scores <- tokens %>%
     sapply(detectR::max_from_list) %>%
     matrix(ncol = 1)
 
+  # Create a list object to do matching on
   matching <- detectR::parse_words(
     tokens = tokens,
     args = args
@@ -32,41 +31,14 @@ features <- function(args) {
 
   # Check for doubled words like `make set` etc..
   matching %<>% lapply(
-    FUN = function(x) x %>% double_words(dbls = dbls)
+    FUN = function(x) x %>% detectR::double_words(dbls = dbls)
   )
 
-  # Sapply over the 3 attack vectors
-  counts <- sapply(
-    X = 1:(attackWords %>% length),
-    FUN = function(x) {
-      atks <- matching %>%
-        purrr::map("wordList") %>%
-        purrr::map(attackWords[[x]]$has_keys)
-
-      # Update attack tokens
-      toRepl <- atks %>%
-        lapply(any) %>%
-        purrr::flatten_lgl() %>%
-        which
-
-      # If any exist, then replace the correct tokens
-      replLen <- toRepl %>% length
-      if (replLen > 0) {
-        for (i in 1:replLen) {
-          ind <- toRepl[i]
-          matching[[ind]]$tokenList[matching[[ind]]$indexes[atks[[ind]]]] <<-
-            if (x == 1) 'x' else if (x == 2) 'y' else 'z'
-        }
-      }
-
-      # Return the attack counts as a vector of the full matrix
-      atks %>%
-        purrr::map(sum) %>%
-        purrr::flatten_dbl()
-    }
-  )
-  # Create a matrix from the counts
-  counts %<>% matrix(ncol = 3)
+  # Replace and count all attack words with specialised tokens
+  counts <- matching %>%
+    detectR::attack_counts(
+      attackWords = detectR::attack_keywords()
+    )
 
   # Shouldn't need to collapse them (but keep for now)
   escores <- matching %>%
