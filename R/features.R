6c464f066cfd9f5b7301ab27d3744f0895a50758
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-20 16:08:18 +0100

    Use different matched object and map a little differently.

--- R/features.R
+++ R/features.R
@@ -5,7 +5,7 @@
 features <- function(args) {
 
   # Initialise attack keywords
-  detectR::attack_keywords()
+  attackWords <- detectR::attack_keywords()
   attackNames <- attackWords %>% names
 
   # Tokenize stuff
@@ -25,41 +25,53 @@ features <- function(args) {
     args = args
   )
 
+  # Define double word combinations
+  dbls <- detectR::doubles$doublewords %>%
+    as.character %>%
+    strsplit(split = " ")
+
   # Check for doubled words like `make set` etc..
-  matching %<>% detectR::double_words()
+  matching %<>% lapply(
+    FUN = function(x) x %>% double_words(dbls = dbls)
+  )
 
   # Sapply over the 3 attack vectors
-  counts <- sapply(1:(attackWords %>% length), function(x) {
-    atks <- purrr::map(matching$wordList, attackWords[[x]]$has_keys)
+  counts <- sapply(
+    X = 1:(attackWords %>% length),
+    FUN = function(x) {
+      atks <- matching %>%
+        purrr::map("wordList") %>%
+        purrr::map(attackWords[[x]]$has_keys)
 
-    # Update attack tokens
-    toRepl <- atks %>%
-      lapply(any) %>%
-      purrr::flatten_lgl() %>%
-      which
+      # Update attack tokens
+      toRepl <- atks %>%
+        lapply(any) %>%
+        purrr::flatten_lgl() %>%
+        which
 
-    # If any exist, then replace the correct tokens
-    replLen <- toRepl %>% length
-    if (replLen > 0) {
-      for (i in 1:replLen) {
-        ind <- toRepl[i]
-        matching$tokenList[[ind]][matching$indexes[[ind]][atks[[ind]]]] <<-
-          if (x == 1) 'x' else if (x == 2) 'y' else 'z'
+      # If any exist, then replace the correct tokens
+      replLen <- toRepl %>% length
+      if (replLen > 0) {
+        for (i in 1:replLen) {
+          ind <- toRepl[i]
+          matching[[ind]]$tokenList[matching[[ind]]$indexes[atks[[ind]]]] <<-
+            if (x == 1) 'x' else if (x == 2) 'y' else 'z'
+        }
       }
-    }
 
-    # Return the attack counts as a vector of the full matrix
-    atks %>%
-      purrr::map(sum) %>%
-      purrr::flatten_dbl()
-  })
+      # Return the attack counts as a vector of the full matrix
+      atks %>%
+        purrr::map(sum) %>%
+        purrr::flatten_dbl()
+    }
+  )
   # Create a matrix from the counts
   counts %<>% matrix(ncol = 3)
 
-  # Shouldn't need to collapse them...
-  collapse <- function(x) x %>% paste(collapse = '')
-  escores <- matching$tokenList %>%
-    lapply(collapse) %>%
+  # Shouldn't need to collapse them (but keep for now)
+  escores <- matching %>%
+    purrr::map("tokenList") %>%
+    lapply(function(x) x %>% paste(collapse = ''))
     sapply(detectR::max_from_list) %>%
     matrix(ncol = 1)
 
