033c20197bb286e24a043a111843e0406a9790ea
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-22 12:23:18 +0100

    Initialise subSum.. was keeping the old index and therefore currentInd was being returned as a zero

--- src/max.f90
+++ src/max.f90
@@ -1,11 +1,11 @@
   subroutine maxinter(vec, vLen, currentInd)
 
   implicit real*8(A-H,O-Z)
-  integer :: j, currentInd, currentSum = 0, subSum, vLen, subsetVal = 4
+  integer :: j, currentInd, currentSum = 0, subSum = 0, vLen, subsetVal = 4
   double precision :: vec(1:vLen)
 
   ! Loop over finding the max score within e.g. [1, 2, 3, 4, 5]
-  do j = 1, (vLen - subsetVal)
+  do j = 1,(vLen - subsetVal)
     subSum = sum(vec(j:(j + subsetVal)))
 
     ! Track the current index
