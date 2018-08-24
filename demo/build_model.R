ddca0192093a68d71799c08ae0594f7919766adc
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-24 20:08:27 +0100

    Add helpful log statements

--- demo/build_model.R
+++ demo/build_model.R
@@ -1,14 +1,19 @@
+# Silently load packages
 library(detectR, warn.conflicts = F, quietly = T)
 library(magrittr, warn.conflicts = F, quietly = T)
+library(crayon, warn.conflicts = F, quietly = T)
 
+cat(crayon::magenta(" --{ Building Neural Network Model }-- \n\n "))
 
 # Make sure the normal data set is built
-fName <- getwd() %>% paste0("/data/d.normal.rda")
+fName <- getwd() %>%
+  paste0("/data/d.normal.rda")
 
 # Build it, and load it
-d.normal <- if (fName %>% file.exists) {
-  detectR::d.normal
+if (fName %>% file.exists) {
+  d.normal <- detectR::d.normal
 } else {
+  cat(crayon::cyan(" --{ Building normal data \n"))
   detectR::prepare()
   load(file = fName)
 }
@@ -22,28 +27,25 @@ d.set <- rbind(
 )
 
 # Build the feature sets
+cat(crayon::cyan(" --{ Building feature set"))
 d.features <- d.set$argument %>%
   detectR::features()
+cat(crayon::cyan(" }-- \n"))
 
 # Append the labels onto the data set
 d.features$label <- d.set$label
 
-emptyFeats <- d.features %>% apply(
-  MARGIN = 2,
-  FUN = function(x) x %>% unique %>% length
-) %>% as.double
-
-if (emptyFeats %>% `==`(1) %>% any) {
-  d.features %<>% subset(select = names(d.features) %>% `[`(emptyFeats %>% `!=`(1) %>% which))
-}
-
 # Build the model ...
+cat(crayon::cyan(" --{ Building model \n"))
 results <- d.features %>%
   detectR::builder(
     normalData = 2000,
     percent = 80
   )
+cat(crayon::cyan(" }-- \n"))
 
 # ... and save it
+cat(crayon::cyan(" --{ Saving to /data/"))
 save(results$nn, file = getwd() %>% paste0("/data/nn.rda"))
 save(results$dataScales, file = getwd() %>% paste0("/data/dataScales.rda"))
+cat(crayon::cyan(" }-- \n"))
