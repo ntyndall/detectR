89c68bbd02b5d78b93652af7dbd57a86a63b9fe0
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-24 16:13:16 +0100

    Function to create data set splitting

---
+++ R/nn_split.R
@@ -0,0 +1,23 @@
+#' @title Neural Network Split Data
+#' @export
+
+
+nn_split <- function(allData, split = 70) {
+
+  # Report on function
+  cat(' \n ## Splitting the data set :', split, '/', 100 %>% `-`(split), '.\n')
+
+  # Create Split (any column is fine)
+  split.data <- allData$N %>%
+    caTools::sample.split(
+      SplitRatio = split %>% `/`(100)
+    )
+
+  # Split based off of split Boolean Vector and return
+  return(
+    list(
+      train = allData %>% subset(split.data %>% `==`(TRUE)),
+      test = allData %>% subset(split.data %>% `==`(FALSE))
+    )
+  )
+}
