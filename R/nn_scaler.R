0d58af66be4d292ed802653de8c5c66b766d25ef
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-24 16:13:43 +0100

    Scale and return the data set

---
+++ R/nn_scaler.R
@@ -0,0 +1,36 @@
+#' @title Neural Network Scale Data
+#' @export
+
+
+nn_scaler <- function(allData, dataScales) {
+
+  # Report on function
+  cat(' \n ## Scaling the data set with : \n',
+      '##    Features :', dataScales$features %>% length, '\n',
+      '##    Labels :', dataScales$labels %>% length, '\n')
+
+  # Use scale() and convert the resulting matrix to a data frame
+  scaled.data <- allData[ , dataScales$features] %>%
+    scale(
+      center = dataScales$minScale,
+      scale =  dataScales$maxScale %>% `-`(dataScales$minScale)
+    ) %>%
+    as.data.frame
+
+  # Set up zerod matrix and allocate the matched labels
+  dSet <- matrix(
+    data = 0,
+    ncol = dataScales$labels %>% length,
+    nrow = allData %>% nrow
+  ) %>%
+    as.data.frame
+  names(dSet) <- dataScales$labels
+
+  for (i in 1:(dSet %>% ncol)) {
+    repl <- allData$label %>% `==`(dataScales$labels[i])
+    if (repl %>% any) dSet[repl %>% which, i] <- 1
+  }
+
+  # Bind the classes to the scaled data and return
+  dSet %<>% cbind(scaled.data) %>% return()
+}
