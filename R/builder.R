102a875cd85484282c0a56ffd6ca9babdf9643e7
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-25 12:03:51 +0100

    Allow any kind of input!

--- R/builder.R
+++ R/builder.R
@@ -3,29 +3,53 @@
 #' @export
 
 
-builder <- function(...) {
-  cat(crayon::magenta(" --{ Building Neural Network Model }-- \n\n "))
+builder <- function(posClass = "N", normalData = 2000, percent = 80, ...) {
+  # Log header details first
+  cat(crayon::magenta(" --{ Building Neural Network Model }-- \n "))
 
-  # Make sure the normal data set is built
-  fName <- getwd() %>%
-    paste0("/data/d.normal.rda")
+  # Check input first
+  input <- list(...)
 
-  # Build it, and load it
-  if (fName %>% file.exists) {
-    d.normal <- detectR::d.normal
+  # Arrange the data set
+  d.set <- if (input %>% length %>% `>`(0)) {
+    # Get data frame names
+    dataNames <- input %>%
+      purrr::map(names)
+
+    # Make sure they are the same length
+    dLen <- dataNames %>%
+      purrr::map(length) %>%
+      purrr::flatten_dbl()
+
+    if (dLen %>% `!=`(2) %>% any) {
+      stop(crayon::red(" ## Ensure all data frames have only 2 columns."))
+    }
+
+    # Make sure all column names are the same, even if they are already
+    for (i in input) names(i) <- c("argument", "label")
+    input %>% purrr::reduce(rbind)
   } else {
-    cat(crayon::cyan(" --{ Building normal data \n"))
-    detectR::prepare()
-    load(file = fName)
-  }
+    # Make sure the normal data set is built
+    fName <- getwd() %>%
+      paste0("/data/d.normal.rda")
 
-  # Load in all data sets
-  d.set <- rbind(
-    d.normal,
-    detectR::d.sqli,
-    detectR::d.xss,
-    detectR::d.bash
-  )
+    # Build it, and load it
+    if (fName %>% file.exists) {
+      d.normal <- detectR::d.normal
+    } else {
+      cat(crayon::cyan(" --{ Building normal data \n"))
+      detectR::prepare()
+      load(file = fName)
+    }
+
+    # Load in all data sets
+    rbind(
+      d.normal,
+      detectR::d.sqli,
+      detectR::d.xss,
+      detectR::d.bash
+    )
+  }
 
   # Build the feature sets
   cat(crayon::cyan(" --{ Building feature set"))
@@ -40,8 +64,9 @@ builder <- function(...) {
   cat(crayon::cyan(" --{ Building model \n"))
   results <- d.features %>%
     detectR::nn_creator(
-      normalData = 2000,
-      percent = 80
+      posClass = posClass,
+      normalData = normalData,
+      percent = percent
     )
   cat(crayon::cyan(" }-- \n"))
 
