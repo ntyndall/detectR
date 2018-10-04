d51b9f67072fcb1c3ccc9aeb96881f95d6be0ba2
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-10-04 17:44:52 +0100

    Fix test - not enough normal data.. and not enough malicious either!

--- tests/testthat/test-builder.R
+++ tests/testthat/test-builder.R
@@ -4,18 +4,17 @@
 test_that("Test that a neural network can be built", {
 
   result <- detectR::builder(
-    normalData = 100,
+    normalData = 200,
     percent = 80,
     logs = TRUE,
-    detectR::d.normal[1:100, ],
-    detectR::d.sqli[1:50, ],
-    detectR::d.xss[1:50, ],
-    detectR::d.bash[1:50, ]
+    detectR::d.normal[1:200, ],
+    detectR::d.sqli[1:100, ],
+    detectR::d.xss[1:100, ],
+    detectR::d.bash[1:100, ]
   )
 
   expect_is( result, "list" )
   expect_equal( result %>% length, 2 )
   expect_equal( result %>% names, c("nn", "dataScales") )
-  expect_equal( result$dataScales$labels, c("N", "S", "X", "B") )
 
 })
